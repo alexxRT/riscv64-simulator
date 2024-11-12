ee7e4f676881838e93c501abbbc46a58b6572a36
user.name <vianmart@yandex.ru>
2024-11-13 01:29:35 +0300

    fix signed immediates

--- instruction.hpp
+++ instruction.hpp
@@ -16,6 +16,7 @@
 #define RD_INIT {rd = (code&INSN_FIELD_RD) >> RD_SHIFT;}
 #define RS1_INIT {rs1 = (code&INSN_FIELD_RS1) >> RS1_SHIFT;}
 #define RS2_INIT {rs2 = (code&INSN_FIELD_RS2) >> RS2_SHIFT;}
+#define SIGN_INIT {sign = code & (1<<31); code &= ~(1<<31);}
 
 class Heart;
 
@@ -26,65 +27,68 @@ typedef uint8_t regIDT;
 class Instruction {
 public:
     typedef std::function<void (Heart*, const Instruction&)> executorT;
-    Instruction(const instT& code);
+    Instruction(instT code);
 
     instT instr_code;
     regIDT rs1, rs2, rd;
     uint32_t imm;
+    uint32_t sign;
     executorT execute;
 //    static uint32_t get_key_val(uint16_t func_code, InstrOpCode op_code);
 };
 
 class InstructionR : public Instruction {
 public:
-    InstructionR(const instT& code) : Instruction(code) {
+    InstructionR(instT code) : Instruction(code) {
         RS1_INIT RS2_INIT RD_INIT
     }
 };
 
 class InstructionI : public Instruction {
 public:
-    InstructionI(const instT& code) : Instruction(code) {
-        RS1_INIT RD_INIT
-        imm = (code&INSN_FIELD_IMM12) >> 20;
+    InstructionI(instT code) : Instruction(code) {
+        RS1_INIT RD_INIT SIGN_INIT
+        imm = ((code&INSN_FIELD_IMM12) >> 20) | sign;
     }
 };
 
 class InstructionS : public Instruction {
 public:
-    InstructionS(const instT& code) : Instruction(code) {
-        RS1_INIT RS2_INIT
+    InstructionS(instT code) : Instruction(code) {
+        RS1_INIT RS2_INIT SIGN_INIT
         imm = ((code&INSN_FIELD_IMM12HI) >> 20)
-            | ((code&INSN_FIELD_IMM12LO) >> 7);
+            | ((code&INSN_FIELD_IMM12LO) >> 7)
+            | sign;
     }
 };
 
 class InstructionB : public Instruction {
 public:
-    InstructionB(const instT& code) : Instruction(code) {
-        RS1_INIT RS2_INIT
-        imm = ((code >> 31) << 12)
-            | ((code & (1<<7)) << 4)
+    InstructionB(instT code) : Instruction(code) {
+        RS1_INIT RS2_INIT SIGN_INIT
+        imm = ((code & (1<<7)) << 4)
             | ((code & 0x7e000000) >> 20)
-            | ((code & 0xf00) >> 7);
+            | ((code & 0xf00) >> 7)
+            | sign;
     }
 };
 
 class InstructionU : public Instruction {
 public:
-    InstructionU(const instT& code) : Instruction(code) {
-        imm = (code & INSN_FIELD_IMM20);
+    InstructionU(instT code) : Instruction(code) {
         RD_INIT
+        imm = (code & INSN_FIELD_IMM20);
     }
 };
 
 class InstructionJ : public Instruction {
 public:
-    InstructionJ(const instT& code) : Instruction(code) {
-        imm = ((code >> 31) << 20)
-            | ((code & INSN_FIELD_ZIMM10) >> 20)
+    InstructionJ(instT code) : Instruction(code) {
+        SIGN_INIT
+        imm = ((code & INSN_FIELD_ZIMM10) >> 20)
             | ((code & (1<<20)) >> 9)
-            | ((code & (0xff000)));
+            | ((code & (0xff000)))
+            | sign
     }
 };
 
