4cec3c8e80dcd1c223bfb522cbe29f9b14cc5a43
nerett <pt-mvb@inbox.lv>
2024-11-13 16:38:33 +0300

    changed reader according to new Heart format

--- elf_reader.hpp
+++ elf_reader.hpp
@@ -1,7 +1,6 @@
 #ifndef ELF_READER_H
 #define ELF_READER_H
 
-#include <iostream>
 #include <elfio/elfio.hpp>
 #include "heart.hpp"
 
@@ -13,8 +12,8 @@ enum class ReaderStatus : int {
 
 class ElfReader {
     public:
-        ElfReader(std::string filename): reader(), file_loaded(false) {
-            file_loaded = reader.load(filename);
+        ElfReader(std::string filename): reader(), file_loaded_(false) {
+            file_loaded_ = reader.load(filename);
         };
 
         ReaderStatus load_instructions(Heart& heart) {
@@ -22,11 +21,14 @@ class ElfReader {
                 return ReaderStatus::BAD_FILE;
             }
 
+            char* data = nullptr;
             uint64_t segment_start = 0, segment_end = 0;
             for (const auto& segment : reader.segments) {
                 if (segment->get_type() == ELFIO::PT_LOAD && (segment->get_flags() & ELFIO::PF_X)) {
                     segment_start = segment->get_virtual_address();
                     segment_end = segment_start + segment->get_memory_size();
+
+                    data = const_cast<char*>(segment->get_data());
                     break;
                 }
             }
@@ -36,35 +38,26 @@ class ElfReader {
                     uint64_t section_start = section->get_address();
                     uint64_t section_end = section_start + section->get_size();
 
-                    if (section_start >= segment_start && section_end <= segment_end) {
-                        size_t size = section->get_size();
-                        const char* data = section->get_data();
+                    if (data && section_start >= segment_start && section_end <= segment_end) {
+                        heart.memory = reinterpret_cast<uint8_t*>(data);
+                        heart.pc = section_start;
 
-                        heart.pc = heart.exec_instructions.size();
-                        heart.exec_instructions.resize(size);
-                        const int* instructions = reinterpret_cast<const int*>(data);
-                        for (size_t i = 0; i < size; ++i) {
-                            heart.exec_instructions.push_back(instructions[i]);
-                        }
-
-                        break;
+                        return ReaderStatus::SUCCESS;
                     }
-
-                    return ReaderStatus::BAD_SEGMENT;
                 }
             }
 
-            return ReaderStatus::SUCCESS;
+            return ReaderStatus::BAD_SEGMENT;
         }
 
         bool file_loaded() {
-            return file_loaded;
+            return file_loaded_;
         }
 
         ELFIO::elfio reader;
 
     private:
-        bool file_loaded;
+        bool file_loaded_;
 }
 
 #endif //ELF_READER_H
