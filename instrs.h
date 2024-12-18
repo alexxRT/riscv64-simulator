934da752ed4a934cde62a8e369372972e37140fe
user.name <vianmart@yandex.ru>
2024-12-18 12:09:39 +0300

    a;most done jit

--- instrs.h
+++ instrs.h
@@ -33,7 +33,7 @@
 #define FIV_BITS ((1<<5)-1)
 
 
-#define JIT_BIN_IU(name) LSET(RD, builder.Create#name(LC64(IMM), LGET(RS1)));
+#define JIT_BIN_IU(name) LSET(RD, builder.Create##name(LC64(IMM), LGET(RS1)));
 
 _INSTR_(SLTI, I, {CODE_BIN_IS(<)}, true, { JIT_BIN_IU(ICmpSLT) })
 _INSTR_(SLTIU, I, {CODE_BIN_IU(<)}, true, { JIT_BIN_IU(ICmpULT) })
@@ -64,31 +64,32 @@ _INSTR_(SLTU, R, {CODE_BIN_RU(<)}, true, { JIT_BIN_OP(ICmpULT) })
 _INSTR_(AND, R, {CODE_BIN_RU(&)}, true, { JIT_BIN_OP(And) })
 _INSTR_(OR, R, {CODE_BIN_RU(|)}, true, { JIT_BIN_OP(Or) })
 _INSTR_(XOR, R, {CODE_BIN_RU(^)}, true, { JIT_BIN_OP(Xor) })
-_INSTR_(SLL, R, {SET_REG(RD, REG(RS1) << (REG(RS2) & SIX_BITS))}, true, {}) // TODO check
-_INSTR_(SRL, R, {SET_REG(RD, (REG(RS1) >> (REG(RS2) & SIX_BITS)))}, true, {}) // TODO check
-_INSTR_(SRA, R, {SET_REG(RD, ((int64_t)REG(RS1) >> (REG(RS2) & SIX_BITS)))}, true, {}) // TODO check
+_INSTR_(SLL, R, {SET_REG(RD, REG(RS1) << (REG(RS2) & SIX_BITS))}, true, { LSET(RD, builder.CreateShl(LGET(RS1), builder.CreateAnd(LGET(RS2), LC64(SIX_BITS)))) }) // TODO check
+_INSTR_(SRL, R, {SET_REG(RD, (REG(RS1) >> (REG(RS2) & SIX_BITS)))}, true, { LSET(RD, builder.CreateLShr(LGET(RS1), builder.CreateAnd(LGET(RS2), LC64(SIX_BITS)))) }) // TODO check
+_INSTR_(SRA, R, {SET_REG(RD, ((int64_t)REG(RS1) >> (REG(RS2) & SIX_BITS)))}, true, { LSET(RD, builder.CreateAShr(LGET(RS1), builder.CreateAnd(LGET(RS2), LC64(SIX_BITS)))) }) // TODO check
 _INSTR_(SLLW, R, {SET_REG(RD, (uint32_t)REG(RS1) << (REG(RS2) & FIV_BITS))}, true, {}) // TODO check
 _INSTR_(SRLW, R, {SET_REG(RD, ((uint32_t)REG(RS1) >> (REG(RS2) & FIV_BITS)))}, true, {}) // TODO check
 _INSTR_(SRAW, R, {SET_REG(RD, ((int32_t)REG(RS1) >> (REG(RS2) & FIV_BITS)))}, true, {}) // TODO check
 _INSTR_(SUB, R, {CODE_BIN_RU(-)}, true, { JIT_BIN_OP(Sub) })
 _INSTR_(SUBW, R, {SET_REG(RD, (int64_t)(int32_t)(REG(RS1) - REG(RS2)))}, true, {}) // TODO check if it's correct but it should be nice
 _INSTR_(ADDW, R, {SET_REG(RD, (int64_t)(int32_t)(REG(RS1) + REG(RS2)))}, true, {}) // TODO check if it's correct but it should be nice
-_INSTR_(JAL, J, { NEW_PC = OPC + IMM; SET_REG(RD, OPC + 4); }, false, {  })
-_INSTR_(JALR, I, { NEW_PC = (IMM + REG(RS1)) & ~1ULL; SET_REG(RD, OPC + 4); }, false, {})
-_INSTR_(BEQ, B, {CODE_CJU(==)}, false, {})
-_INSTR_(BNE, B, {CODE_CJU(!=)}, false, {})
-_INSTR_(BLT, B, {CODE_CJS(<)}, false, { \
-BasicBlock *trueBr = BasicBlock::Create(ctx, "", fn); \
+_INSTR_(JAL, J, { NEW_PC = OPC + IMM; SET_REG(RD, OPC + 4); }, false, { builder.CreateStore(new_pc, builder.CreateAdd(LPC, LC64(IMM))); LSET(RD, builder.CreateAdd(LPC, LC64(4)));  })
+_INSTR_(JALR, I, { NEW_PC = (IMM + REG(RS1)) & ~1ULL; SET_REG(RD, OPC + 4); }, false, { builder.CreateStore(new_pc, builder.CreateAnd( builder.CreateAdd(LGET(RS1), LC64(IMM)), LC64(~1ULL))); LSET(RD, builder.CreateAdd(LPC, LC64(4))); })
+
+#define JIT_COND_JMP(name) BasicBlock *trueBr = BasicBlock::Create(ctx, "", fn); \
 BasicBlock *retBr = BasicBlock::Create(ctx, "", fn); \
-builder.CreateCondBr(builder.CreateICmpSLT(LGET(RS1), LGET(RS2)), trueBr, retBr); \
+builder.CreateCondBr(builder.CreateICmp##name(LGET(RS1), LGET(RS2)), trueBr, retBr); \
 builder.SetInsertPoint(trueBr); \
-builder.CreateStore(builder.CreateAdd(builder.CreateLoad(Type::getInt64Ty(ctx), pc), LC64(IMM)), new_pc); \
+builder.CreateStore(builder.CreateAdd(LPC, LC64(IMM)), new_pc); \
 builder.CreateBr(retBr); \
-builder.SetInsertPoint(retBr); \
-})
-_INSTR_(BLTU, B, {CODE_CJU(<)}, false, {})
-_INSTR_(BGE, B, {CODE_CJS(>=)}, false, {})
-_INSTR_(BGEU, B, {CODE_CJU(>=)}, false, {})
+builder.SetInsertPoint(retBr);
+
+_INSTR_(BEQ, B, {CODE_CJU(==)}, false, { JIT_COND_JMP(EQ) })
+_INSTR_(BNE, B, {CODE_CJU(!=)}, false, { JIT_COND_JMP(NE) })
+_INSTR_(BLT, B, {CODE_CJS(<)}, false, { JIT_COND_JMP(SLT) })
+_INSTR_(BLTU, B, {CODE_CJU(<)}, false, { JIT_COND_JMP(ULT) })
+_INSTR_(BGE, B, {CODE_CJS(>=)}, false, { JIT_COND_JMP(SGE) })
+_INSTR_(BGEU, B, {CODE_CJU(>=)}, false, { JIT_COND_JMP(UGE) })
 _INSTR_(ADDIW, I, {SET_REG(RD, (int64_t)(int32_t)(REG(RS1) + IMM))}, true, {}) // TODO check if it's correct but it should be nice
 // TODO check type conversions
 _INSTR_(LD, I, { SET_REG(RD, *(uint64_t*)MEM(REG(RS1)+IMM)); }, false, {})
